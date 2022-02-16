f79ac88987993408fb1f674a8704765d526f77f0
Steyn Kalala <steynmkalala@gmail.com>
2022-02-16 10:37:12 -0500

    first commit

--- pages/user/become-instructor.js
+++ pages/user/become-instructor.js
@@ -45,7 +45,7 @@ const BecomeInstructor = () => {
               <br />
               <h2>Setup payout to publish courses on Edemy</h2>
               <p className="lead text-warning">
-                Edemy partners with stripe to transfer earnings to your bank
+               Skater partners with stripe to transfer earnings to your bank
                 account
               </p>
 
